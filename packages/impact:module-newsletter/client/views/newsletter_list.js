6f0813e677afc3b58b76bd90e0b4df1db4872dc4
Piotr Migdal <pmigdal@gmail.com>
2014-10-11 23:32:09 +0200

    newsletter listing views

--- packages/impact:module-newsletter/client/views/newsletter_list.js
+++ packages/impact:module-newsletter/client/views/newsletter_list.js
@@ -1,9 +1,12 @@
 Template.newsletter_newsletter_list.helpers({
 
   'newsletters': function () {
-    return Modules.Newsletter.Newsletters.find({}, {
-      sort: {createdAt: -1}
-    });
+    return Modules.Newsletter.Newsletters
+      .find({}, {sort: {createdAt: -1}})
+      .map(function (each, index) {
+        each.index = index + 1;  // human-readable index, starting from
+        return each;
+      });
   },
 
   'subscriberCount': function (newsletter_id) {
--- packages/impact:module-newsletter/client/views/subscriber_list.js
+++ packages/impact:module-newsletter/client/views/subscriber_list.js
@@ -1,5 +1,17 @@
-Template.newsletter_subscriber_list.subscribers = function() {
-  return Modules.Newsletter.Subscribers.find({}, {
-    sort: {createdAt: -1}
-  });
-};
+Template.newsletter_subscriber_list.helpers({
+
+  'subscribers': function () {
+    return Modules.Newsletter.Subscribers
+        .find({}, {sort: {createdAt: -1}})
+        .map(function (each, index) {
+          each.index = index + 1;  // human-readable index, starting from 1
+          return each;
+        });
+  },
+
+  'length': function (arr) {
+    return arr.length;
+  },
+
+});
+
