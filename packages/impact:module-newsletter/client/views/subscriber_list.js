7bd82f5e012a421dcf0ed40236c3938f84c26c2a
Piotr Migdal <pmigdal@gmail.com>
2014-10-13 13:15:47 +0200

    fixed form reloading, sorting by update

--- packages/impact:module-newsletter/client/views/subscriber_list.js
+++ packages/impact:module-newsletter/client/views/subscriber_list.js
@@ -2,7 +2,7 @@ Template.newsletter_subscriber_list.helpers({
 
   'subscribers': function () {
     return Modules.Newsletter.Subscribers
-        .find({}, {sort: {createdAt: -1}})
+        .find({}, {sort: {updatedAt: -1}})
         .map(function (each, index) {
           each.index = index + 1;  // human-readable index, starting from 1
           return each;
