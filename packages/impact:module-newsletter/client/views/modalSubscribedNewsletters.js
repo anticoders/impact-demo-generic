8c4d2291f61ec335f32ec5318243ffdc4eb62844
Piotr Migdal <pmigdal@gmail.com>
2014-10-19 14:23:41 +0200

    array to mongo find to make modal reactive

--- packages/impact:module-newsletter/client/views/modalSubscribedNewsletters.js
+++ packages/impact:module-newsletter/client/views/modalSubscribedNewsletters.js
@@ -3,14 +3,15 @@ Template.newsletter_modalSubscribedNewsletters.helpers({
   // I don't know how to pass subscriberId below ("click .fa-remove")
   'newslettersEnriched': function () {
     var subscriberId = this._id;
-    return this.newsletters.map(function (each) {
-      var newsletter = Modules.Newsletter.Newsletters.findOne(each);
-      newsletter = newsletter || {name: "(not existing)"};  // removable, but my current data is bad
-      return {
-        subscriberId: subscriberId,
-        newsletterId: each,
-        newsletterName: newsletter.name
-      }
+    return Modules.Newsletter.Subscribers.findOne(subscriberId).newsletters
+      .map(function (each) {
+        var newsletter = Modules.Newsletter.Newsletters.findOne(each);
+        newsletter = newsletter || {name: "(not existing)"};  // removable, but my current data is bad
+        return {
+          subscriberId: subscriberId,
+          newsletterId: each,
+          newsletterName: newsletter.name
+        };
     });
   },
 
