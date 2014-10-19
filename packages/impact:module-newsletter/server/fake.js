3fe503d78a7375103031c99c5f7a5e734aa3af6a
Piotr Migdal <pmigdal@gmail.com>
2014-10-19 16:20:34 +0200

    faking emails

--- packages/impact:module-newsletter/server/fake.js
+++ packages/impact:module-newsletter/server/fake.js
@@ -40,7 +40,22 @@ _.times(40, function() {
   });
 });
 
-// fake emails
+_.times(20, function() {
+
+  var sentAt = moment().valueOf() + 1000 * 60 * 60 * 24 * 3 * (Math.random() - 0.5);
+
+  Modules.Newsletter.Emails.insert({
+    title:       Fake.sentence(_.sample([2, 2, 3])),
+    content:     Fake.sentence(_.sample([20, 40, 60, 100])),
+    newsletter:  _.sample(newsletterIds),
+
+    sentAt:    sentAt,
+    sent:      sentAt < moment().valueOf(),
+
+    createdAt: moment().valueOf(),
+    updatedAt: moment().valueOf(),
+  });
+});
 
 // QUESTION:
 // Aren't we duplicating dates from observe.js?
