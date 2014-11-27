6de8d7e6056fb64350a1035d46d13400bd926f6c
Hubert OG <hubert@orlikarnia.com>
2014-11-27 14:58:09 +0100

    Some semantic improvements

---
+++ packages/anti:semantic-autoform/package.impact.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary:  'Semantic templates for Autoform',
+  name:     'anti:semantic-autoform',
+  version:  '0.0.0',
+});
+
+Package.onUse(function (api, where) {
+  
+  //-- IMPACT: FILES --//
+
+});
